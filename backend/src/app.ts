76ed7c7749886ce5eed04252050682b95237f8f3
Halyna Hryn <71042425+GGalina@users.noreply.github.com>
2025-11-13 22:07:04 +0200

    UPDATED: App.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
